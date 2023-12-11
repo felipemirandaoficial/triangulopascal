e16df8282dc342d284416e859d7a6f81700de238
Felipe Miranda <143961165+felipemirandaoficial@users.noreply.github.com>
2023-12-11 16:26:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+<script>
+        function calcularTrianguloPascal(rows) {
+            var triangle = [];
+
+            for (var i = 0; i < rows; i++) {
+                triangle[i] = [];
+                triangle[i][0] = 1;
+
+                for (var j = 1; j < i; j++) {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+
+                triangle[i][i] = 1;
+            }
+
+            return triangle;
+        }
+
+        function exibirTrianguloPascal(triangle) {
+            var pascoso;
+            for (var i = 0; i < triangle.length; i++) {
+                for (var j = 0; j < triangle[i].length; j++) {
+                    pascoso += '\n' + triangle[i][j] ;
+                }
+            }
+            console.log(pascoso);
+        }
+
+        var numRows = 6;
+        var pascalTriangle = calcularTrianguloPascal(numRows);
+        exibirTrianguloPascal(pascalTriangle);
+    </script>
